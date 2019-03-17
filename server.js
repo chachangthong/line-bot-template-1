a0917ae7c34b23f389fee7318069adc41163287b
bilanphoto <bilanphoto.lik9@gmail.com>
2019-03-18 00:52:26 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,41 +1,51 @@
+
+
+
+
+
+
+// Reply with two static messages
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
-
-app.set('port', (process.env.PORT || 5000)) 
-
+const port = process.env.PORT || 4000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
 
 var Token = process.env.TOKEN || "xxx";
 var access_token = 'Bearer {'+Token+'}'
 
+
 app.get('/',function (req, res) {
     res.end("ok bot")
     })
-
 app.post('/webhook', (req, res) => {
-  var msgbot = req.body.events[0].message.text
-  var replyToken = req.body.events[0].replyToken  
-
+     var tex_t = req.body.events[0].message.text
+     var sende_r = req.body.events[0].source.userId
+     var replytoke_n = req.body.events[0].replyToken
+    
+    console.log("---0---");
+    console.log(tex_t);
+    
+    let reply_token = req.body.events[0].replyToken
+    reply(reply_token)
     res.sendStatus(200)
-    console.log(msgbot);
-    sendText(replyToken, msgbot)
+    res.end("ok")
 })
-
-/////////////////////////////////////
-
-function sendText(reply_token, msgbot) {
+app.listen(port)
+function reply(reply_token) {
     let headers = {
         'Content-Type': 'application/json',
-        'Authorization': access_token
+        'Authorization': 'Bearer ' + Token
     }
     let body = JSON.stringify({
         replyToken: reply_token,
         messages: [{
             type: 'text',
-            text: msgbot
+            text: 'Hello'
         }]
     })
     request.post({
@@ -46,9 +56,3 @@ function sendText(reply_token, msgbot) {
         //console.log('status = ' + res.statusCode);
     });
 }
-
-
-app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
-})
-
