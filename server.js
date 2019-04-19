05b681b7d5627b92031b38950e01c649f6769976
chachangthong <49773794+chachangthong@users.noreply.github.com>
2019-04-19 11:38:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.listen(port)
 function reply(reply_token) {
     let headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'b316afc916aeae77f04c89988efabcda'
+        'Authorization': '{b316afc916aeae77f04c89988efabcda}'
     }
     let body = JSON.stringify({
         replyToken: reply_token,
