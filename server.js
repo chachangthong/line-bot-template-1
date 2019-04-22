e8601a94c8ca9ed92373f8a3fa3149a3bbf0837c
chachangthong <49773794+chachangthong@users.noreply.github.com>
2019-04-22 11:47:00 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,32 +2,12 @@ const express = require('express');
 const line = require('@line/bot-sdk');
 const http = require('http');
 
-var path = require("path");//+++
-var bodyParser = require("body-parser");//+++
-var mongodb = require("mongodb");//+++
-var ObjectID = mongodb.ObjectID;//+++
-var CONTACTS_COLLECTION = "contacts";//+++
 
 
 require('dotenv').config();
 
 const app = express();
-app.use(express.static(__dirname + "/public")); //+++
-app.use(bodyParser.json());//+++
 
-var db;//+++
-
-
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-    
-      db = database;
-  console.log("Database connection ready");
-
-  });
     
 ////////////////////////////////////////////////////////////////////////////////////////////
 const config = {
@@ -37,6 +17,10 @@ const config = {
 
 const client = new line.Client(config);
 
+app.get('/',function (req, res) {
+    res.end("ok bot")
+    })
+
 app.post('/webhook', line.middleware(config), (req, res) => {
     Promise
         .all(req.body.events.map(handleEvent))
@@ -44,87 +28,7 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 });
 ////////////////////////////////////////////////////////////////////////////////////////////    
     
-    
-    
-    
-    
-    
-    app.get("/contacts", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
-    if (err) {
-      handleError(res, err.message, "Failed to get contacts.");
-    } else {
-      res.status(200).json(docs);
-    }
-  });
-});
-    
- app.post("/contacts", function(req, res) {
-  var newContact = req.body;
-  newContact.createDate = new Date();
 
-  if (!(req.body.firstName || req.body.lastName)) {
-    handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
-  }
-
-  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to create new contact.");
-    } else {
-      res.status(201).json(doc.ops[0]);
-    }
-  });
-});
-  
-  
-  
-  
-  
-  app.get("/contacts/:id", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to get contact");
-    } else {
-      res.status(200).json(doc);
-    }
-  });
-});
-  
-  
-  
-  
-  
-  app.delete("/contacts/:id", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
-    if (err) {
-      handleError(res, err.message, "Failed to delete contact");
-    } else {
-      res.status(204).end();
-    }
-  });
-});
-  
-  
-  
-  
-  
-  
-  
-  app.put("/contacts/:id", function(req, res) {
-  var updateDoc = req.body;
-  delete updateDoc._id;
-
-  db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to update contact");
-    } else {
-      res.status(204).end();
-    }
-  });
-});
-    
-    
-    
     
     
     
