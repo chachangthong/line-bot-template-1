e4ac74c36ccf4f4d83d2fa48278a95d286c43745
chachangthong <49773794+chachangthong@users.noreply.github.com>
2019-04-23 16:00:09 +0700

    Update server.js

--- server.js
+++ server.js
@@ -112,13 +112,17 @@ var name = profile.displayName;
 var picture = profile.pictureUrl;
 //request('https://docs.google.com/forms/u/2/d/1iUGX58guFhU3bkt1OglhOGoDuv5i6mPQAs35gy4IOcw/formResponse?ifq&entry.1691916586='+event.source.userId+'&entry.556749397='+name+'&entry.1687867422='+picture+'&entry.66040433=data1&entry.1800492209=data2&entry.53513319=data3&entry.1987831678=data4&submit=Submit');	
 
-ref.push(data) 
-	var data = { 
+var data = { 
 name : {
     name : profile.displayName,  
     userId: event.source.userId,   
   }
-}
+}	
+	
+	
+	
+ref.push(data) 
+	
 	
 	
 /*	
